cd49de6a80dfa003078d488f1e3afa15d1a898b9
xiaohu <xiaohu@qq.com>
2022-09-14 17:16:22 +0800

    lodash

---
+++ lodash/h_rookie-lodash.js
@@ -0,0 +1,11 @@
+var h_rookie = {
+  indexOf: function indexOf(ary, target) {
+    for (var i = 0; i < ary.length; i++) {
+      if (ary[i] === target) {
+        return i
+      }
+    }
+
+    return -1
+  }
+}
